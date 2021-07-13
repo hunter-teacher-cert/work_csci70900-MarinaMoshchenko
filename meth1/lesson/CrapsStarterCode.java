4ffe794c86c521dd9355cf0abcae178a6a852c79
Marina Moshchenko <marina.moschenko@gmail.com>
2021-07-13 12:20:23 -0400

    Starter Code for Craps

---
+++ meth1/lesson/CrapsStarterCode.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.util.Random;
+
+//TEAM: /YOUR NAMES ARE HERE/
+
+public class Craps{
+	
+	public static void main(String[] args){
+		//call your methods here
+		}
+
+	//method "roll" should accept an integer parameter and returns a random number between 1 and that number, inclusive
+	public static int roll(int dice){
+		//your code apperas here
+		}
+	
+	
+	
+	//method "shoot" should accepts two parameters - a number of dice and the maximum value for those dice and returns the result of rolling those dice
+	public static int shoot( int numOfDice, int max){
+			//your code appers here
+		return result;
+	}
+		
+	//method "round" should accept no parameters and it should return something to indicate if the shooter of the round wins or loses
+	public static int round(){
+			//your code appears here
+	}
+		
+	
+	
+	
+	
+	
+	/* 
+	The shooter shoots (rolls) the dice.
+
+If he rolls a 2,3, or 12,that’s Craps and he loses.
+If he rolls a 7 or 11, that’s a Natural and he wins.
+Otherwise:
+
+the value he rolled is now called the Point
+the shooter continues to shoot (roll) until he either rolls the Point again at which point he wins or he rolls a 7 at which point he loses.
+*/
+	}
+	
+	
+	

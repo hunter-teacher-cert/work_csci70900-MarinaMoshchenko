f54f4ffcc5f185b262bc36aa956fc3c024f73be8
MarinaMoshchenko <marina.moschenko@gmail.com>
2021-07-26 08:18:20 -0400

    Comments

--- ds/tree/part1/TreeNode.java
+++ ds/tree/part1/TreeNode.java
@@ -6,38 +6,38 @@ public class TreeNode{
     private TreeNode left,right; //object with data, left pointer and right pointer
 
     public TreeNode(){ //creates and object when the method is invoked
-	this(0);	//data is 0
-	left = null;	//both pointers refer to null
-	right = null;
-    }
+		this(0);	//data is 0
+		left = null;	//both pointers refer to null
+		right = null;
+		}
 
     public TreeNode(int data){ //creates an object with specific data
-	this.data = data;
-	left = null;	//both pointers refer to null
-	right = null;
-    }
+		this.data = data;
+		left = null;	//both pointers refer to null
+		right = null;
+		}
 
     public void setData(int data){		//sets the data only
-	this.data = data;
-    }
+		this.data = data;
+		}
 
-    public void setLeft(TreeNode left){	//sets left pointer only
-	this.left = left;
-    }
+		public void setLeft(TreeNode left){	//sets left pointer only
+		this.left = left;
+		}
 
     public void setRight(TreeNode right){	//sets the right pointer only
-	this.right = right;
-    }
+		this.right = right;
+		}
 
-    public int getData(){		//gets the value of a specific node
-	return data;
-    }
+		public int getData(){		//gets the value of a specific node
+		return data;
+		}
 
     public TreeNode getLeft(){	//gets the left pointer
-	return left;
-    }
+		return left;
+		}
 
-    public TreeNode getRight(){		//gets the right pointer
-	return right;
-    }
-}
+		public TreeNode getRight(){		//gets the right pointer
+		return right;
+		}
+	}

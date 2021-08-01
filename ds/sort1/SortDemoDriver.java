55b1358ae7c2a4f2c9445a9004e251aee037f9e8
MarinaMoshchenko <marina.moschenko@gmail.com>
2021-08-01 03:15:36 -0400

    Sort, Linear, Binary

--- ds/sort1/SortDemoDriver.java
+++ ds/sort1/SortDemoDriver.java
@@ -15,18 +15,17 @@ public class SortDemoDriver {
 	System.out.println("sd["+ i + "] = "+sd.get(i)+" : " + sd);
 	i = sd2.findSmallestIndex(0);
 	System.out.println("sd2["+ i + "] = "+sd2.get(i)+" : " + sd2);
-
+*/
 
 	  System.out.println(sd);
-	  sd.sort();
+	  sd.sort(); 
 	  System.out.println(sd);
 	  
-	  //does not work
-*/	  
-	 //int linResult = sd.linearSearch(5);
-	 //int binResult = sd.binarySearch(5);	//iterative
-	 int binrResult = sd.binarySearch(5, 0, 14);
 	 
-   
+	 int linResult = sd.linearSearch(5);
+	 int binResult = sd.binarySearch(5);	//iterative
+	 int binrResult = sd.binarySearch(5, 0, 14);	//recursive
+	 
+ 
     }
 }

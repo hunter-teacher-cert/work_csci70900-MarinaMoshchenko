1c406b374bf9c858f9edbf7fc28e4e9ad7183b9a
MarinaMoshchenko <marina.moschenko@gmail.com>
2021-08-06 11:27:23 -0400

    Knights Tour

---
+++ ds/knights/Driver.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+    public static void main(String[] args) {
+	boolean solved;
+	
+
+	
+	
+	  Knights k = new Knights(5);
+	  solved = k.solve(4,4,1);
+
+	
+    }
+}
